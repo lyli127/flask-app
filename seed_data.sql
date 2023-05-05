a57c0a99660da822460d2dfbb4124ca64fbef225
lyli127 <lylibete@gmail.com>
2023-05-05 21:34:24 +1000

    file name update

--- seed_data.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS mytable;
-
-CREATE TABLE mytable(id SERIAL PRIMARY KEY, item TEXT);
-INSERT INTO mytable(item) VALUES('groceries');
-INSERT INTO mytable(item) VALUES('laundry');
-INSERT INTO mytable(item) VALUES('walk the dog');
-INSERT INTO mytable(item) VALUES('buy gifts');
-
-SELECT * FROM mytable;
