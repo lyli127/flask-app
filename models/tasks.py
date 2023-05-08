7c45af1c0a86dc40683be026e1ababc650cad529
lyli127 <lylibete@gmail.com>
2023-05-08 11:08:47 +1000

    write tasks func

--- models/tasks.py
+++ models/tasks.py
@@ -16,3 +16,12 @@ def sql_read_tasks(query, parameters=[]):
     return results
 
 
+def sql_write_tasks(query, parameters=[]):
+    """Function Works!"""
+    # connection
+    connection = psycopg2.connect(os.getenv("DATABASE_URL"))
+    cursor = connection.cursor()
+    cursor.execute(query, parameters)
+    connection.commit()
+    connection.close()
+
